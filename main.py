2fb96dc2d5bbbc8827bd30f48cdbf9e9b2048dba
Yuriy Bandura <bandurayv@gmail.com>
2024-10-25 20:15:34 +0300

    Add_Step_3

--- main.py
+++ main.py
@@ -1,27 +1,5 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from app import app
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '1234'
-
-class NameForm(FlaskForm): # Используем FlaskForm в качестве родительского класса
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        return redirect(url_for('hello', name=name))
-    return render_template('index.html', form=form)
-
-@app.route('/user/<name>')
-def hello(name):
-    return f'<h1>Hello, {name}!</h1>'
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
 
