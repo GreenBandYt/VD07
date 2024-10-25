4704e0c9c792e340e23ee9155f7caa331055057e
Yuriy Bandura <bandurayv@gmail.com>
2024-10-25 17:39:09 +0300

    Add_Step_2

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '1234'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db' # Эта строчка нужна, чтобы мы могли подключиться к базе данных
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Эта строчка отключает сигнализацию об изменении объектов внутри базы данных
+class NameForm(FlaskForm): # Используем FlaskForm в качестве родительского класса
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
-db = SQLAlchemy(app) # Создание объекта, через который мы будем работать с базой данных
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        return redirect(url_for('hello', name=name))
+    return render_template('index.html', form=form)
 
-class User(db.Model): # В скобках указываем модель, чтобы в дальнейшем создать именно базу данных
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False) # В скобках описываем поля таблицы
-
-    def __repr__(self): # Этот метод определяет, как объект модели будет выглядеть в виде строки
-        return f'<User {self.username}>'
-
-with app.app_context(): # Функция создаёт контекст приложения, который нужен для работы с базой данных
-    db.create_all() # Создание всей таблицы, которые определены в классе User
-
-@app.route('/add_user') # С помощью декоратора создаём маршрут, который будет вызывать функцию
-def add_user(): # Функция будет создавать объект класса User
-    new_user = User(username='new_username')
-    db.session.add(new_user) # Добавляем в сессию
-    db.session.commit() #  Сохраняем изменения в базу данных
-    return 'User added' # Вывод сообщения о том, что юзер добавлен в базу данных
-
-@app.route('/users')
-def get_users():
-    users = User.query.all() # Получаем всех юзеров из базы данных и сохраняем в переменную users
-    return str(users)
+@app.route('/user/<name>')
+def hello(name):
+    return f'<h1>Hello, {name}!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
